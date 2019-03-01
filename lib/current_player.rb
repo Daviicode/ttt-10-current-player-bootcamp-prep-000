6d353ff2412915339d100054946daf8982835961
David zhang <d.ydzdavid@yahoo.com>
2019-03-01 12:56:17 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,15 @@
 
 
-def turn_count (board, token = "X")
-  board.each do |token|
+def turn_count (board)
+  counter = 0
+  board.each do |count|
+    puts #{count}
+  counter += 1  
+end
     
     
     
-    
-def current_player(turn_count)  
+def current_player(turn_count, token = "X")  
   if turn_count % 2 == 0 
     return "O"
   else 
